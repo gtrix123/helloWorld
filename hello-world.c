6164c938b101220e276ef4f0d355ccfaf36c9c21
Gayatri S <gsusarl1@jhu.edu>
2015-09-21 19:05:11 -0400

    Repeat n times

--- hello-world.c
+++ hello-world.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
+  int i = 0; // interator 
+  int n = 5; //max 
+  // repeat n times 
+  for( i = 0; i < n; i++) { 	
   printf("Hello World!\n");
+} 
 
-  printf("argc = %d/n", argc); 
+  // printf("argc = %d/n", argc); 
 
   return EXIT_SUCCESS; 
 } 
